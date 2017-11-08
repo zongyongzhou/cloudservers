1a748e110280d3b6ceb854ee813cd8c705b5a888
zyz <374515529@qq.com>
2017-11-08 17:29:31 +0800

    cloud server

---
+++ src/main/java/com/example/server/ConfigServersApplication.java
@@ -0,0 +1,12 @@
+package com.example.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServersApplication.class, args);
+	}
+}
